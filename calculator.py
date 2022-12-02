368ecf615ac12699c5349e11d8903e7ce72b534e
Albatrozx11 <97524402+Albatrozx11@users.noreply.github.com>
2022-12-02 20:43:16 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def(a,b,op):
+  add=a+b
+  sub=a-b
+  mul=a*b
+  div=a/b
+  if op=='+':
+    return add
+  elif op=='-':
+    return sub
+  elif op=='*':
+    return mul
+  elif op=='div':
+    return div
+  
+  
+    
